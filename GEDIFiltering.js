93c4d34ec7c7393d2196746ecbdb1380a745c41e
alexlecka <66777855+alexlecka@users.noreply.github.com>
2023-03-29 13:05:59 +0200

    Update GEDIFiltering.js
    added comments

--- GEDIFiltering.js
+++ GEDIFiltering.js
@@ -1,11 +1,11 @@
+// filter out pixels with quality flag 0 and degrade flag 1
 exports.qualityMask = function(img) {
-  // to get rid of pixels with quality flag 0 and degrade flag 1
   return img.updateMask(img.select('quality_flag').eq(1))
     .updateMask(img.select('degrade_flag').eq(0));
 };
 
+// filter out observations acquired by the coverage beam of GEDI
 exports.beamMask = function(img) {
-  // retain only pixels corresponding to full power beams
   var beam5 = img.select('beam').eq(5);
   var beam6 = img.select('beam').eq(6);
   var beam8 = img.select('beam').eq(8);
@@ -15,14 +15,14 @@ exports.beamMask = function(img) {
   return img.updateMask(beam);
 };
 
+// filter out observations made during the day
 exports.nightMask = function(img) {
-  // retain only pixel corresponding to negative angles (night measurements)
   var night = img.select('solar_azimuth').lt(0);
   return img.updateMask(night);
 };
 
+// filter out observations from outside the leaf-on season
 exports.leafMask = function(img) {
-  // retain only pixels from measurement within the leafy season
   var leaf = img.select('leaf_on_cycle').eq(1);
   return img.updateMask(leaf);
 };
